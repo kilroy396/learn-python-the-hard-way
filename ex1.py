63770b231a26688a65c2220c60a0fff9620d2288
kilroy396 <kilroy396@users.noreply.github.com>
2017-07-07 12:58:25 +1000

    Excerise 1 file created
    Based on  https://learnpythonthehardway.org/python3/ex1.html

---
+++ ex1.py
@@ -0,0 +1,9 @@
+# based on https://learnpythonthehardway.org/python3/ex1.html
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
