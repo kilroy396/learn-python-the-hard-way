3dcaad93256e57bb00c6f365cb7d487e1f1fe9f8
kilroy396 <andrew@boistout.net>
2017-07-17 14:10:06 +1000

    updated exercise 14 and .gitignore

--- exercises/ex14.py
+++ exercises/ex14.py
@@ -1,14 +1,19 @@
 from sys import argv
 
-script, user_name = argv
-prompt = '> '
+script, user_name, gender = argv
+prompt = '-> '
 
-print(f"Hi {user_name}, I'm the {script} script.")
+if(gender == 'male'):
+    salutation = "Mr"
+else:
+    salutation = "Mrs"
+
+print(f"Hi {salutation} {user_name}, I'm the {script} script.")
 print("I'd like to ask you a few questions.")
 print(f"Do you like me {user_name}?")
 likes = input(prompt)
 
-print(f"Where do you like {user_name}?")
+print(f"Where do you live {user_name}?")
 lives = input(prompt)
 
 print(f"What kind of computer do you have?")
